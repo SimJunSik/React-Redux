4785c672705d0e55f5d302ebf5a0da96686dac08
SimJunSik <wnstlr24@naver.com>
2019-12-25 09:17:36 +0900

    React.memo() , useCallback() 내용 추가

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import Todos from '../components/Todos';
 import { addTodo, toggleTodo } from '../modules/todos';
 
+/*
+    useCallback => 랜더링 할 때 마다 매번 새로 만드는게 아니라 재사용하고 싶을 때
+*/
 function TodosContainer(){
     const todos = useSelector(state => state.todos);
     const dispatch = useDispatch();
